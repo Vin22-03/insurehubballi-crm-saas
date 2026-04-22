b100320431606679cc2184d9f5fe67a355ba4ae3
Vin22-03 <vinay.venvin@gmail.com>
2026-04-22 22:08:28 +0530

    chenged prisma adapter to mariadb, added bcryptjs.

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: "srv917.hstgr.io",
+  port: 3306,
+  user: "u674178439_insurecrm",
+  password: "Insure@crm43",
+  database: "u674178439_insure_crm",
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
